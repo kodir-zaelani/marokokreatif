bf91e389769cce93f81085e0d89dfb9860a07733
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-09-03 08:46:26 +0800

    edit route

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,8 @@ Route::view('dashboard', 'dashboard')
 ->middleware(['auth', 'verified'])
 ->name('dashboard');
 
-Auth::routes(
-    [
-        'register' => false
-        ]
-    );
     
-    Route::middleware(['auth'])->group(function () {
+    Route::middleware(['auth', 'register' => false])->group(function () {
         Route::redirect('settings', 'settings/profile');
         
         Route::get('settings/profile', Profile::class)->name('settings.profile');
