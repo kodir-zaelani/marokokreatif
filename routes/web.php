bad8a024b1a0504e031b2a3171e1975d9ba443f9
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-09-03 08:14:03 +0800

    add frontend controller

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [App\Http\Controllers\Frontend\FrontendController::class, 'index'])->name('root');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
