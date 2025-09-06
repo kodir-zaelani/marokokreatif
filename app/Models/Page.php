58a2b9c2a6236f2a94f4b28d1c02924f6fb3ee38
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-09-06 22:40:03 +0800

    add model page

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -10,6 +11,8 @@ class Page extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
+
     protected $table        = 'pages';
     protected $primaryKey   = 'id';
     protected $guarded      = [];
